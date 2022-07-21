848cceabef2c008deb6f334bbb234fc578584982
Alex Sandri <me@alexsandri.com>
2022-07-21 13:01:28 +0200

    add docs

--- lib/resolve_font_height.dart
+++ lib/resolve_font_height.dart
@@ -1,7 +1,11 @@
+/// Flutter package to get the default font's height based on the current build context.
 library resolve_font_height;
 
 import 'package:flutter/material.dart';
 
+/// Returns the default font's height based on the [context] and [fontSize].
+///
+/// If no [fontSize] is provided the default one will be used.
 double resolveFontHeight(BuildContext context, [double? fontSize]) {
   final double height = DefaultTextStyle.of(context).style.height! *
       (fontSize ?? DefaultTextStyle.of(context).style.fontSize!) *
