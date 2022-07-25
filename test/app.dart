1db0773619d309281df18856305981af981b5777
Alex Sandri <me@alexsandri.com>
2022-07-25 20:02:49 +0200

    add text scale factors test cases

--- test/app.dart
+++ test/app.dart
@@ -3,8 +3,13 @@ import 'package:resolve_font_height/resolve_font_height.dart';
 
 class App extends StatelessWidget {
   final double? fontSize;
+  final double? textScaleFactor;
 
-  const App({super.key, this.fontSize});
+  const App({
+    super.key,
+    this.fontSize,
+    this.textScaleFactor,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +17,23 @@ class App extends StatelessWidget {
       title: 'Test',
       theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
-        body: Column(
-          children: [
-            SizedBox(height: resolveFontHeight(context, fontSize)),
-            Text('Test', style: TextStyle(fontSize: fontSize)),
-          ],
+        body: Builder(
+          builder: (context) {
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaleFactor: textScaleFactor),
+              child: Builder(
+                builder: (context) {
+                  return Column(
+                    children: [
+                      SizedBox(height: resolveFontHeight(context, fontSize)),
+                      Text('Test', style: TextStyle(fontSize: fontSize)),
+                    ],
+                  );
+                },
+              ),
+            );
+          },
         ),
       ),
     );
--- test/resolve_font_height_test.dart
+++ test/resolve_font_height_test.dart
@@ -8,22 +8,38 @@ void main() {
     "resolveFontHeight() matches Text widget's height",
     (tester) async {
       const List<double?> fontSizes = [null, 18, 30, 50];
+      const List<double?> textScaleFactors = [0.8, null, 1.2, 1.5, 2];
 
       for (final double? fontSize in fontSizes) {
-        await tester.pumpWidget(App(fontSize: fontSize));
-
-        final textFinder = find.text('Test');
-        final sizedBoxFinder = find.byType(SizedBox);
-
-        expect(textFinder, findsOneWidget);
-        expect(sizedBoxFinder, findsOneWidget);
-
-        final textHeight = textFinder.evaluate().single.size!.height;
-        final sizedBoxHeight =
-            (sizedBoxFinder.evaluate().single.widget as SizedBox).height;
-
-        expect(textHeight, sizedBoxHeight);
+        for (final double? textScaleFactor in textScaleFactors) {
+          await test(tester, fontSize, textScaleFactor);
+        }
       }
     },
   );
+}
+
+Future<void> test(
+  WidgetTester tester,
+  double? fontSize,
+  double? textScaleFactor,
+) async {
+  await tester.pumpWidget(
+    App(
+      fontSize: fontSize,
+      textScaleFactor: textScaleFactor,
+    ),
+  );
+
+  final textFinder = find.text('Test');
+  final sizedBoxFinder = find.byType(SizedBox);
+
+  expect(textFinder, findsOneWidget);
+  expect(sizedBoxFinder, findsOneWidget);
+
+  final textHeight = textFinder.evaluate().single.size!.height;
+  final sizedBoxHeight =
+      (sizedBoxFinder.evaluate().single.widget as SizedBox).height;
+
+  expect(textHeight, sizedBoxHeight);
 }
